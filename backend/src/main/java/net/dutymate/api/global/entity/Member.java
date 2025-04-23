2a0e394a0036cdbd52a603ae82020832ca2dd78d
MINSSUNGKIM <planman0@gmail.com>
2025-04-23 19:39:08 +0900

    [S12P31A202-74] feat: Add generating count to member entity

--- backend/src/main/java/net/dutymate/api/global/entity/Member.java
+++ backend/src/main/java/net/dutymate/api/global/entity/Member.java
@@ -87,6 +87,9 @@ public class Member {
 	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<BoardLikes> boardLikesList;
 
+	@Column(nullable = false)
+	private Integer autoGenCnt;
+
 	// 멤버 초기값 설정 (닉네임, 생성시각, 활성화여부)
 	@PrePersist
 	public void prePersist() {
@@ -94,6 +97,7 @@ public class Member {
 		this.createdAt = new Timestamp(System.currentTimeMillis());
 		this.isActive = true;
 		this.isVerified = true;		// 신규 회원은 이메일 인증된 상태로 간주
+		this.autoGenCnt = 5;
 	}
 
 	public void changeAdditionalInfo(Integer grade, Gender gender, Role role) {
