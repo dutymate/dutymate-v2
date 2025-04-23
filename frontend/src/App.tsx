08a3cd2b90f1c9224587cf08db63cdc806a654e5
MINSSUNGKIM <planman0@gmail.com>
2025-04-23 16:38:02 +0900

    [S12P31A202-66] feat: Connect channel talk service

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import Router from "./routes/Router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageLoadingSpinner from "./components/atoms/Loadingspinner";
+import ChannelTalkLoader from "./components/organisms/ChannelTalkLoader";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<ChannelTalkLoader />
 			<PageLoadingSpinner />
 			<Router />
 			<ToastContainer
