02ba0439251932fb6fd3d4b5d6172214f87c2bb0
한종우 <jongwooo.han@gmail.com>
2025-05-07 20:30:13 +0900

    [S12P31A202-313] chore: Add egress configuration for http

--- infrastructure/Modules/SecurityGroup/main.tf
+++ infrastructure/Modules/SecurityGroup/main.tf
@@ -107,6 +107,13 @@ resource "aws_security_group" "sg_appserver_ecs" {
   }
 
   egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
