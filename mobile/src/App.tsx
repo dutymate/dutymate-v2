1972ea54e787281984038dd6ac7b555be0e9b636
한종우 <jongwooo.han@gmail.com>
2025-05-12 10:18:10 +0900

    [S12P31A202-437] feat: Add navigation container configurations

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,15 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 
-import { WebViewContainer } from "@/components/webview/WebViewContainer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { LandingScreen } from "@/screens/LandingScreen";
+import { WebViewScreen } from "@/screens/WebViewScreen";
+
+const Stack = createStackNavigator();
 
 /**
  * 앱의 메인 컴포넌트입니다.
+ * 이 컴포넌트는 React Navigation을 사용하여 앱의 내비게이션을 설정합니다.
  */
 export default function App() {
 	return (
 		<>
 			<StatusBar style="auto" />
-			<WebViewContainer />
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName={"Landing"}
+					screenOptions={{ headerShown: false }}
+				>
+					<Stack.Screen name="Landing" component={LandingScreen} />
+					<Stack.Screen name="WebView" component={WebViewScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
 		</>
 	);
 }
