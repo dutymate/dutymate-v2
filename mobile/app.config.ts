b0e1394dbf61bfe4e232913a0d62365c14a736f1
한종우 <jongwooo.han@gmail.com>
2025-05-13 15:10:18 +0900

    [S12P31A202-505] chore: Add tutorialUrl and youtubeUrl to app.config.ts

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -6,6 +6,8 @@ export default ({ config }) => {
                 process.env.APP_ENV === "production"
                     ? "https://dutymate.net"
                     : "http://localhost:5173",
+            tutorialUrl: process.env.EXPO_PUBLIC_TUTORIAL_URL,
+            youtubeUrl: process.env.EXPO_PUBLIC_YOUTUBE_URL,
         },
     };
 };
