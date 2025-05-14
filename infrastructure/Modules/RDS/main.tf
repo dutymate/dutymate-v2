3fe874136083eb06c0ad310806e118e5cf3b228f
한종우 <jongwooo.han@gmail.com>
2025-05-14 09:57:47 +0900

    [S12P31A202-526] chore: Set backup_retention_period to 7

--- infrastructure/Modules/RDS/main.tf
+++ infrastructure/Modules/RDS/main.tf
@@ -8,20 +8,21 @@ resource "aws_db_subnet_group" "dbsg" {
 }
 
 resource "aws_db_instance" "db" {
-  allocated_storage      = 20
-  engine                 = "mysql"
-  engine_version         = "8.0.40"
-  instance_class         = "db.t3.micro"
-  username               = var.mysql_username
-  password               = var.mysql_password
-  identifier             = "dutymate-db"
-  multi_az               = false
-  db_subnet_group_name   = aws_db_subnet_group.dbsg.name
-  vpc_security_group_ids = [var.sg_mysql_id]
-  parameter_group_name   = aws_db_parameter_group.db_params.name
-  kms_key_id             = var.kms_rds_key_arn
-  storage_encrypted      = true
-  skip_final_snapshot    = true
+  allocated_storage       = 20
+  engine                  = "mysql"
+  engine_version          = "8.0.40"
+  instance_class          = "db.t3.micro"
+  username                = var.mysql_username
+  password                = var.mysql_password
+  identifier              = "dutymate-db"
+  multi_az                = false
+  db_subnet_group_name    = aws_db_subnet_group.dbsg.name
+  vpc_security_group_ids  = [var.sg_mysql_id]
+  parameter_group_name    = aws_db_parameter_group.db_params.name
+  kms_key_id              = var.kms_rds_key_arn
+  storage_encrypted       = true
+  skip_final_snapshot     = true
+  backup_retention_period = 7
 
   tags = {
     Name = "dutymate-db"
