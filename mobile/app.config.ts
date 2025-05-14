92372b162c92729b088fbcd0acd1185ebe62e3b5
한종우 <jongwooo.han@gmail.com>
2025-05-14 18:46:43 +0900

    [S12P31A202-555] refactor: Use app.config.ts instead of app.json

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,6 +1,32 @@
-export default ({ config }) => {
+import { ExpoConfig, ConfigContext } from "@expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
     return {
         ...config,
+        name: "dutymate-mobile",
+        slug: "dutymate-mobile",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        userInterfaceStyle: "light",
+        newArchEnabled: true,
+        splash: {
+            image: "./assets/splash-icon.png",
+            resizeMode: "contain",
+            backgroundColor: "#ffffff"
+        },
+        ios: {
+            supportsTablet: true
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/adaptive-icon.png",
+                backgroundColor: "#ffffff"
+            }
+        },
+        web: {
+            favicon: "./assets/favicon.png"
+        },
         extra: {
             url:
                 process.env.APP_ENV === "production"
