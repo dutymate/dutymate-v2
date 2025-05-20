368fdde1927659b2e86012a418ec6725203d52e9
한종우 <jongwooo.han@gmail.com>
2025-05-20 11:41:39 +0900

    [S12P31A202-726] feat: Add keywords to seo

--- frontend/src/components/SEO.tsx
+++ frontend/src/components/SEO.tsx
@@ -8,10 +8,38 @@ interface SEOProps {
   ogUrl?: string;
 }
 
+const baseKeywords = [
+  'Dutymate',
+  '듀티메이트',
+  '듀메',
+  '듀티표',
+  '간호',
+  '간호사',
+  '수간호사',
+  '평간호사',
+  '스케줄링',
+  '근무표',
+  '근무 패턴',
+  '자동 생성',
+  '자동화',
+  '병원',
+  '병동',
+  '의료',
+  '데이',
+  '이브닝',
+  '나이트',
+  '오프',
+  '미드',
+  '나오데',
+  '교대 근무',
+  '커뮤니티',
+  '약속 잡기',
+];
+
 export const SEO = ({
   title = 'Dutymate',
-  description = '간호사 업무의 효율성과 공정성을 높이는 자동화 듀티표 생성 서비스',
-  keywords = '듀티메이트, 듀티표, 간호사, 스케줄링, 근무표, 자동생성, 자동화, 병원, 의료',
+  description = '간호사 업무의 효율성과 공정성을 높이는 근무표 자동 생성 서비스',
+  keywords = baseKeywords.join(', '),
   ogImage = '/images/og-image.png',
   ogUrl = 'https://dutymate.net',
 }: SEOProps) => {
